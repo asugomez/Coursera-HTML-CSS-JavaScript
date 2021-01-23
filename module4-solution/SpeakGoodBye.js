91a1cada7ba9312b0e2e90306f1460552153ae67
Asuncion Gomez <asuncion.gomez@ug.uchile.cl>
2021-01-23 18:20:04 +0100

    final solution

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -23,8 +23,6 @@
 
   // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
   // 'byeSpeaker' on the global scope as well.
-  // xxxx.xxxx = byeSpeaker;
   window.byeSpeaker = byeSpeaker;
-
 
 })(window);
